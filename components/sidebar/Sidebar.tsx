e1012ee5633b4ece2300463f680b00d1eeceedd3
Vipul Katwal <vipulkatwal17@gmail.com>
2025-05-21 23:39:47 +0530

    Add lucide-react dependency and refactor Sidebar component for improved UI.

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -1,67 +1,72 @@
 'use client';
-import { FiHome, FiMessageCircle, FiEdit2, FiBarChart2, FiList, FiVolume2, FiZap, FiBook, FiImage, FiCheckSquare, FiSettings, FiStar, FiPlay } from 'react-icons/fi';
-import ChatFilter from './ChatFilter';
-import ChatList from '../chat/ChatList';
-import LabelList from './LabelList';
-import MemberAssign from './MemberAssign';
 
-/**
- * Sidebar component for chat navigation, search, labels, and members.
- * Matches the left panel in the screenshot.
- */
-export default function Sidebar() {
+import { AnalyticsIcon, BroadcastIcon, PeriskopeIcon } from "@/utils/Icons";
+import { IconType } from "react-icons";
+import { IoChatbubbleEllipses, IoTicket } from "react-icons/io5";
+import { FaListUl } from "react-icons/fa";
+import { RiContactsBookFill, RiFolderImageFill } from "react-icons/ri";
+import { MdChecklist } from "react-icons/md";
+import { BsGearFill } from "react-icons/bs";
+import { TbStarsFilled } from "react-icons/tb";
+import { AiFillHome } from "react-icons/ai";
+import { LogOut, Wand2 } from "lucide-react";
+
+interface MenuItem {
+  href?: string;
+  icon?: IconType | React.ComponentType<{ size?: number }>;
+  divider?: boolean;
+  isNew?: boolean;
+  isImplemented?: boolean;
+}
+
+const Sidebar: React.FC = () => {
+  // WhatsApp-style icon order and types
+  const menuItems: MenuItem[] = [
+    { href: "/dashboard", icon: AiFillHome },
+    { href: "/chats", icon: IoChatbubbleEllipses },
+    { href: "/tickets", icon: IoTicket },
+    { href: "/analytics", icon: AnalyticsIcon },
+    { href: "/list", icon: FaListUl },
+    { href: "/broadcast", icon: BroadcastIcon },
+    { href: "/rules", icon: Wand2 }, // Sparkles/magic wand
+    { href: "/contacts", icon: RiContactsBookFill },
+    { href: "/media", icon: RiFolderImageFill },
+    { href: "/logs", icon: MdChecklist },
+    { href: "/settings", icon: BsGearFill },
+  ];
+
   return (
-    <div className="flex h-full">
-      {/* Icon bar */}
-      <div className="flex flex-col items-center bg-[#f6f7f9] w-16 py-4 gap-2 border-r border-gray-200">
-        <div className="mb-4">
-          <img src="/peris.png" alt="Periskope Logo" className="w-10 h-10 rounded-full shadow border-2 border-white bg-white" />
+    <div className="h-screen w-14 flex flex-col justify-between bg-white shadow-md">
+      <div className="flex flex-col gap-y-1 pt-3">
+        <div className="flex justify-center items-center mb-2">
+          <PeriskopeIcon className="h-9 w-9" />
         </div>
-        <SidebarIcon icon={<FiHome size={22} />} />
-        <SidebarIcon icon={<FiMessageCircle size={22} />} />
-        <SidebarIcon icon={<FiEdit2 size={22} />} />
-        <SidebarIcon icon={<FiBarChart2 size={22} />} />
-        <SidebarIcon icon={<FiList size={22} />} />
-        <SidebarIcon icon={<FiVolume2 size={22} />} />
-        <SidebarIcon icon={<FiZap size={22} />} />
-        <SidebarIcon icon={<FiBook size={22} />} />
-        <SidebarIcon icon={<FiImage size={22} />} />
-        <SidebarIcon icon={<FiCheckSquare size={22} />} />
-        <SidebarIcon icon={<FiSettings size={22} />} />
-        <SidebarIcon icon={<FiStar size={22} />} />
-        <SidebarIcon icon={<FiPlay size={22} />} />
+        <div className="flex flex-col items-center gap-1">
+          {menuItems.map((item, index) =>
+            item.divider ? (
+              <hr key={`divider-${index}`} className="border-gray-200 m-1" />
+            ) : (
+              item.href && item.icon && (
+                <SidebarIcon key={item.href} icon={item.icon} />
+              )
+            )
+          )}
+        </div>
       </div>
-      {/* Chat list panel */}
-      <aside className="flex flex-col h-full w-[340px] bg-white rounded-tr-2xl shadow-lg overflow-hidden">
-        {/* Logo at the top, centered */}
-        <div className="flex justify-center pt-6 pb-2">
-          <img src="/peris.png" alt="Periskope Logo" className="w-12 h-12 rounded-full shadow border-2 border-white bg-white" />
-        </div>
-        {/* Top: Custom filter and search */}
-        <div className="px-4 pt-2 pb-2 border-b border-gray-100">
-          <div className="flex items-center justify-between mb-2">
-            <span className="font-semibold text-green-700 text-lg">Custom filter</span>
-          </div>
-          <ChatFilter />
-        </div>
-        {/* Chat list */}
-        <div className="flex-1 overflow-y-auto bg-[#f7f8fa]">
-          <ChatList />
-        </div>
-        {/* Bottom: Labels and member assign */}
-        <div className="p-4 border-t border-gray-100">
-          <LabelList />
-          <MemberAssign />
-        </div>
-      </aside>
+      <div className="flex flex-col items-center gap-1 mb-2">
+        <SidebarIcon icon={TbStarsFilled} />
+        <SidebarIcon icon={LogOut} extraClass="mb-1" />
+      </div>
+    </div>
+  );
+};
+
+function SidebarIcon({ icon: Icon, extraClass = "" }: { icon: React.ComponentType<{ size?: number }>, extraClass?: string }) {
+  return (
+    <div className={`flex items-center justify-center w-10 h-10 rounded-full cursor-pointer text-[#3a3d42] ${extraClass}`}>
+      <Icon size={20} />
     </div>
   );
 }
 
-function SidebarIcon({ icon }: { icon: React.ReactNode }) {
-  return (
-    <div className="flex items-center justify-center w-10 h-10 rounded-full hover:bg-green-50 cursor-pointer text-gray-500 mb-1">
-      {icon}
-    </div>
-  );
-}
+export default Sidebar;
---
+++ components/utils/Icons.tsx
@@ -0,0 +1,237 @@
+export const AnalyticsIcon = (props: any) => (
+  <svg
+    {...props}
+    fill="currentColor"
+    strokeWidth="0"
+    viewBox="0 0 512 512"
+    aria-hidden="true"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M64 64c0-17.7-14.3-32-32-32S0 46.3 0 64V400c0 44.2 35.8 80 80 80H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H80c-8.8 0-16-7.2-16-16V64zm406.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L320 210.7l-57.4-57.4c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L240 221.3l57.4 57.4c12.5 12.5 32.8 12.5 45.3 0l128-128z"></path>
+  </svg>
+);
+
+export const BroadcastIcon = (props: any) => (
+  <svg
+    {...props}
+    fill="currentColor"
+    strokeWidth="0"
+    viewBox="0 0 24 24"
+    aria-hidden="true"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M16.881 4.345A23.112 23.112 0 0 1 8.25 6H7.5a5.25 5.25 0 0 0-.88 10.427 21.593 21.593 0 0 0 1.378 3.94c.464 1.004 1.674 1.32 2.582.796l.657-.379c.88-.508 1.165-1.593.772-2.468a17.116 17.116 0 0 1-.628-1.607c1.918.258 3.76.75 5.5 1.446A21.727 21.727 0 0 0 18 11.25c0-2.414-.393-4.735-1.119-6.905ZM18.26 3.74a23.22 23.22 0 0 1 1.24 7.51 23.22 23.22 0 0 1-1.41 7.992.75.75 0 1 0 1.409.516 24.555 24.555 0 0 0 1.415-6.43 2.992 2.992 0 0 0 .836-2.078c0-.807-.319-1.54-.836-2.078a24.65 24.65 0 0 0-1.415-6.43.75.75 0 1 0-1.409.516c.059.16.116.321.17.483Z"></path>
+  </svg>
+);
+
+export const RulesIcon = (props: any) => (
+  <svg
+    {...props}
+    fill="currentColor"
+    strokeWidth="0"
+    version="1.2"
+    baseProfile="tiny"
+    viewBox="0 0 24 24"
+    aria-hidden="true"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M18 16.184v-1.851c0-1.93-1.57-3.5-3.5-3.5-.827 0-1.5-.673-1.5-1.5v-1.517c1.161-.415 2-1.514 2-2.816 0-1.654-1.346-3-3-3s-3 1.346-3 3c0 1.302.839 2.401 2 2.815v1.518c0 .827-.673 1.5-1.5 1.5-1.93 0-3.5 1.57-3.5 3.5v1.851c-1.161.415-2 1.514-2 2.816 0 1.654 1.346 3 3 3s3-1.346 3-3c0-1.302-.839-2.401-2-2.816v-1.851c0-.827.673-1.5 1.5-1.5.979 0 1.864-.407 2.5-1.058.636.651 1.521 1.058 2.5 1.058.827 0 1.5.673 1.5 1.5v1.851c-1.161.415-2 1.514-2 2.816 0 1.654 1.346 3 3 3s3-1.346 3-3c0-1.302-.839-2.401-2-2.816zm-11 3.816c-.552 0-1-.449-1-1s.448-1 1-1 1 .449 1 1-.448 1-1 1zm5-16c.552 0 1 .449 1 1s-.448 1-1 1-1-.449-1-1 .448-1 1-1zm5 16c-.552 0-1-.449-1-1s.448-1 1-1 1 .449 1 1-.448 1-1 1z"></path>
+  </svg>
+);
+
+export const CollapseIcon = (props: any) => (
+  <svg
+    {...props}
+    fill="currentColor"
+    strokeWidth="0"
+    viewBox="0 0 24 24"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M18 3a3 3 0 0 1 2.995 2.824l.005 .176v12a3 3 0 0 1 -2.824 2.995l-.176 .005h-12a3 3 0 0 1 -2.995 -2.824l-.005 -.176v-12a3 3 0 0 1 2.824 -2.995l.176 -.005h12zm-3 2h-9a1 1 0 0 0 -.993 .883l-.007 .117v12a1 1 0 0 0 .883 .993l.117 .007h9v-14zm-3.293 4.293a1 1 0 0 1 .083 1.32l-.083 .094l-1.292 1.293l1.292 1.293a1 1 0 0 1 .083 1.32l-.083 .094a1 1 0 0 1 -1.32 .083l-.094 -.083l-2 -2a1 1 0 0 1 -.083 -1.32l.083 -.094l2 -2a1 1 0 0 1 1.414 0z"></path>
+  </svg>
+);
+
+export const PeriskopeIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 512 512"
+  >
+    <defs>
+      <clipPath id="periskope-clip">
+        <circle cx="256" cy="256" r="256" />
+      </clipPath>
+    </defs>
+
+    <g clipPath="url(#periskope-clip)">
+      <rect width="512" height="512" fill="#15803D" fillOpacity="1" />
+      <path fill="#fff" d="M88 258c0-55.228 44.772-100 100-100h100v369H88z" />
+      <path
+        fill="#fff"
+        d="M218 158h125c55.228 0 100 44.772 100 100s-44.772 100-100 100H218z"
+      />
+      <circle cx="339" cy="258" r="75" fill="#15803D" fillOpacity="1" />
+      <path
+        fill="#15803D"
+        fillOpacity="1"
+        d="m256.478 322.112 12.879-37.603 26.712 34.08z"
+      />
+      <circle cx="310" cy="261" r="10" fill="#fff" />
+      <circle cx="343" cy="261" r="10" fill="#fff" />
+      <circle cx="376" cy="261" r="10" fill="#fff" />
+    </g>
+  </svg>
+);
+
+export const GenerateIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    stroke="currentColor"
+    fill="currentColor"
+    strokeWidth="0"
+    viewBox="0 0 20 20"
+    aria-hidden="true"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      d="M5 2a1 1 0 011 1v1h1a1 1 0 010 2H6v1a1 1 0 01-2 0V6H3a1 1 0 010-2h1V3a1 1 0 011-1zm0 10a1 1 0 011 1v1h1a1 1 0 110 2H6v1a1 1 0 11-2 0v-1H3a1 1 0 110-2h1v-1a1 1 0 011-1zM12 2a1 1 0 01.967.744L14.146 7.2 17.5 9.134a1 1 0 010 1.732l-3.354 1.935-1.18 4.455a1 1 0 01-1.933 0L9.854 12.8 6.5 10.866a1 1 0 010-1.732l3.354-1.935 1.18-4.455A1 1 0 0112 2z"
+      clipRule="evenodd"
+    ></path>
+  </svg>
+);
+
+export const TextFileIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    fill="currentColor"
+    strokeWidth="0"
+    viewBox="0 0 448 512"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M448 96c0-35.3-28.7-64-64-64L64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320zM256 160c0 17.7-14.3 32-32 32l-96 0c-17.7 0-32-14.3-32-32s14.3-32 32-32l96 0c17.7 0 32 14.3 32 32zm64 64c17.7 0 32 14.3 32 32s-14.3 32-32 32l-192 0c-17.7 0-32-14.3-32-32s14.3-32 32-32l192 0zM192 352c0 17.7-14.3 32-32 32l-32 0c-17.7 0-32-14.3-32-32s14.3-32 32-32l32 0c17.7 0 32 14.3 32 32z"></path>
+  </svg>
+);
+
+export const OverviewIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    fill="currentColor"
+    strokeWidth="0"
+    viewBox="0 0 512 512"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM64 256c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H96c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"></path>
+  </svg>
+);
+
+export const PropertiesIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    stroke="currentColor"
+    fill="none"
+    strokeWidth="2"
+    viewBox="0 0 24 24"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M13 5h8"></path>
+    <path d="M13 9h5"></path>
+    <path d="M13 15h8"></path>
+    <path d="M13 19h5"></path>
+    <path d="M3 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z"></path>
+    <path d="M3 14m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z"></path>
+  </svg>
+);
+
+export const IntegrationIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    stroke="currentColor"
+    fill="none"
+    strokeWidth="2"
+    viewBox="0 0 24 24"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="8" height="8" x="3" y="3" rx="2"></rect>
+    <path d="M7 11v4a2 2 0 0 0 2 2h4"></path>
+    <rect width="8" height="8" x="13" y="13" rx="2"></rect>
+  </svg>
+);
+
+export const MembersIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    stroke="currentColor"
+    fill="currentColor"
+    strokeWidth="0"
+    viewBox="0 0 20 20"
+    aria-hidden="true"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"></path>
+  </svg>
+);
+
+export const GenerateOutlineIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    stroke="currentColor"
+    fill="none"
+    strokeWidth="2"
+    viewBox="0 0 24 24"
+    aria-hidden="true"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
+    ></path>
+  </svg>
+);
+
+export const NewChatIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    stroke="currentColor"
+    fill="none"
+    strokeWidth="2"
+    viewBox="0 0 24 24"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M12.007 19.98a9.869 9.869 0 0 1 -4.307 -.98l-4.7 1l1.3 -3.9c-2.324 -3.437 -1.426 -7.872 2.1 -10.374c3.526 -2.501 8.59 -2.296 11.845 .48c1.992 1.7 2.93 4.04 2.747 6.34"></path>
+    <path d="M16 19h6"></path>
+    <path d="M19 16v6"></path>
+  </svg>
+);
